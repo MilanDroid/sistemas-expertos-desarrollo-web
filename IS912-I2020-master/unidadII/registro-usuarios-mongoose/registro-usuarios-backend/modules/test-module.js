a0672390ea71a813274b6bcd52c3864a4053a60e
MilanDroid <antonybrenescruz@gmail.com>
2020-10-08 21:05:23 -0600

    Configurando rama para almacenar la tarea3

--- IS912-I2020-master/unidadII/registro-usuarios-mongoose/registro-usuarios-backend/modules/test-module.js
+++
@@ -1,4 +0,0 @@
-module.exports.x = 5;
-module.exports.mostrarMensaje = function(){
-    return 'Hola mundo, funcion de un módulo'; 
-}
--- IS912-I2020-master/unidadII/spotify/spotify/backend/models/usuario.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var esquema = new mongoose.Schema(
-    {
-        nombreUsuario: String,
-        playlists: Array
-    }
-);
-
-module.exports = mongoose.model('usuarios', esquema);
