f50beaac9c60ebcbe8b3f1109d093fd5209f4d3a
MilanDroid <antonybrenescruz@gmail.com>
2020-11-25 09:15:32 -0600

    Creando componentes del ejemplo

--- spotifyMilan/frontend/src/app/app.module.ts
+++ spotifyMilan/frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { AlbumComponent } from './components/album/album.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PlaylistComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
